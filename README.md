ef6c93adadda0b55c46f0f786dbb63d55390d762
hymd <hymd@hymdResearch.local>
2019-08-27 21:13:58 +0900

    All refactored.

--- README.md
+++ README.md
@@ -9,21 +9,28 @@ end
 ```
 The function above is going to be evaluated as below.
 ```julia
-function fib(n::T; memo=Dict{Tuple{Vararg{T}}, BigInt}())::BigInt where {T<:Signed}
-    if haskey(memo, (n,))
-        memo[(n,)]
-    else
-        get!(
-            memo,
-            (n,),
-            begin
-              if n<2
-                n
-              else
-                fib(n-1, memo=memo) + fib(n-2, memo=memo)
-              end
+-function fib(n::T; memo=Dict{T, BigInt}())::BigInt where {T<:Signed}
++fib =
++let memo=Dict{$(Expr(:curly, :Tuple, f_args_types...)), BigInt}()
++    function fib(n::T)::BigInt where T<:Signed
++        let args = tuple(n::T,)
+            if haskey(memo, args)
+                memo[args]
+            else
+                get!(
+                    memo,
+                    args,
+-                    begin
+                      if n<2
+                        n
+                      else
+-                        fib(n-1, memo=memo) + fib(n-2, memo=memo)
++                        fib(n-1) + fib(n-1)
+                      end
+-                    end
+                    )
             end
-            )
-    end
++        end
++    end
 end
 ```
--- src/Memoize.jl
+++ src/Memoize.jl
@@ -3,93 +3,56 @@ export @memoize
 
 macro memoize(f)
 
-
-    argnames, argtypes = begin
-        root = f.args[1]
-
-        while root.head!=:call
-            root = root.args[1]
+    head_parser = function(Ex::Union{Expr,Symbol}; head=:call)
+        if typeof(Ex) != Expr
+            return Expr(head, :(nop(nothing)), :Any)
+        elseif Ex.head == head
+            return Ex
+        else
+            return head_parser(Ex.args[1]; head=head)
         end
-
-        args = root.args[2:length(root.args)]
-
-        args, map(ex->typeof(ex)==Expr ? ex.args[2] : :Any, args)
     end
 
 
-    fname, ftype = begin
-        root = f.args[1]
-        fname, ftype = :Symbol, :Symbol
-        while root.head!=:call
-            if root.head == :(::)
-
-                ftype = root.args[2]
-            end
-            root = root.args[1]
-        end
-        fname = root.args[1]
-
-        insert!(
-            root.args,
-            2,
-            Expr(
-                :parameters,
-                Expr(
-                    :kw,
-                    :memo,
-                    Expr(
-                        :call,
-                        Expr(
-                            :curly,
-                            :Dict,
-                            Expr(
-                                :curly,
-                                :Tuple,
-                                argtypes...
-                                ),
-                            ftype
-                            )
-                        )
-                    )
-                )
-        )
-        fname, ftype
+    root = head_parser(f.args[1]; head=:where)
+    f_where = let args = root.args[2]
+        :($args)
     end
 
-    f.args[2] = begin
-        root = f.args[2]
+    root = head_parser(f.args[1]; head=:(::))
+    f_type = root.args[2]
 
-        stack=[root]
-        while !isempty(stack)
-            root = pop!(stack)
-            if typeof(root)==Expr
-                if root.args[1] == fname
-                    push!(root.args, Expr(:kw, :memo, :memo))
-                end
+    root = head_parser(f.args[1]; head=:call)
+    f_name = root.args[1]
+    f_args = root.args[2:end]
 
-                nodes=filter(ex->typeof(ex)==Expr, root.args)
+    f_args_types = map(f_args) do args
+        typeof(args)==Symbol ? :Any : args.args[2]
+    end
 
-                while !isempty(nodes)
-                    push!(stack, pop!(nodes))
+    f_block = let root = f.args[2]
+
+        if typeof(root.args[1])!=Expr
+            root.args[2]
+        else
+            root.args[1]
+        end
+
+    end
+
+    let args=tuple(f_args...)
+        @eval $f_name = let memo = Dict{Tuple{Vararg}, $f_type}()
+            function $f_name($(args[1]), $(args[2:end] ...))::($f_type) where $f_where
+                let tpl = ($(args[1]), $(args[2:end] ...))
+                    if haskey(memo, tpl)
+                        memo[tpl]
+                    else
+                        get!(memo, tpl, $f_block)
+                    end
                 end
             end
         end
-
-        f.args[2]
     end
-
-    f.args[2] =
-    :(
-        let args=tuple($(argnames...))
-            if haskey(memo, args)
-                memo[args]
-            else
-                get!(memo, args, $(f.args[2]))
-            end
-        end
-    )
-
-    @eval $f
 
 end
 end
