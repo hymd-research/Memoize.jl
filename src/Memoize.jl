263e3b2591e1bf08fcdea53cf5bf58a36684c868
Shunsuke Hosoyamada <hymd.email@gmail.com>
2019-12-26 11:34:16 +0900

    Update Memoize.jl

--- src/Memoize.jl
+++ src/Memoize.jl
@@ -1,9 +1,7 @@
 module Memoize
-export @memoize
+export @memoize, @dumpf
 
-macro memoize(f)
-
-    f_parser = function(Ex::Union{Expr,Symbol}; head=:call)
+f_parser = function(Ex::Union{Expr,Symbol}; head=:call)
         if typeof(Ex) != Expr
             return Expr(head, :(nop(nothing)), :Any)
         elseif Ex.head == head
@@ -12,6 +10,8 @@ macro memoize(f)
             return f_parser(Ex.args[1]; head=head)
         end
     end
+
+macro memoize(f)
 
 
     Fwhere = let args = f_parser(f.args[1]; head=:where).args[2]
@@ -57,4 +57,46 @@ macro memoize(f)
     
 end
 
+macro dumpf(exf::Expr)
+    
+    Fwhere = let args = f_parser(f.args[1]; head=:where).args[2]
+        :($args)
+    end
+
+    fn, fargs = let root = f_parser(f.args[1]; head=:call)
+        root.args[1], root.args[2:end]
+    end
+
+    OutType = f_parser(f.args[1]; head=:(::)).args[2]
+    InTypes = map(fargs) do arg
+        typeof(arg)==Symbol ? :Any : arg.args[2]
+    end
+
+    block = let root = f.args[2]
+
+        if typeof(root.args[1])!=Expr
+            root.args[2]
+        else
+            root.args[1]
+        end
+
+    end
+    
+    expr = let args = tuple(fargs...)
+        :(
+            $fn = let memo = Dict{Tuple{Vararg}, $OutType}()
+                function $fn($(args...))::($OutType) where $Fwhere
+                    let tpl = tuple($(args...))
+                        if haskey(memo, tpl)
+                            memo[tpl]
+                        else 
+                            get!(memo, tpl, $block)
+                        end
+                    end
+                end
+            end
+        )
+    end
+    
+    dump(expr)
 end
