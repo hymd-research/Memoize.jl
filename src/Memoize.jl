fa9e9d6f2327b770ef91b5a16859655d3fca278f
Shunsuke Hosoyamada <hymd.email@gmail.com>
2019-12-26 13:06:19 +0900

    Update Memoize.jl

--- src/Memoize.jl
+++ src/Memoize.jl
@@ -2,8 +2,10 @@ module Memoize
 export @memoize, @dumpf, @showf
 
 f_parser = function(Ex::Union{Expr,Symbol}; head=:call)
+    
+    show(Ex);println();println(typeof(Ex))
+    
     if typeof(Ex) != Expr
-        show(Ex);println()
         return Expr(:call, :nop(nothing), :Nothing)
     elseif Ex.head == head
         return Ex
